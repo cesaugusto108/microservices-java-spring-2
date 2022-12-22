d92066f08f5a331bddffeb523ece1ba5cc36b6f3
César Augusto <ces.augusto108@gmail.com>
2022-12-22 16:23:07 -0300

    academic_sys module created

---
+++ academic_sys/src/main/java/ces/augusto108/academic_sys/application/AcademicSysApplication.java
@@ -0,0 +1,11 @@
+package ces.augusto108.academic_sys.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicSysApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicSysApplication.class, args);
+	}
+}
---
+++ academic_sys/src/test/java/ces/augusto108/academic_sys/AcademicSysApplicationTests.java
@@ -0,0 +1,13 @@
+package ces.augusto108.academic_sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
