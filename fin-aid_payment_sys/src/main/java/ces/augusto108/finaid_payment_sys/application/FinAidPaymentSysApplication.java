2156b0055a2ee5a3667968953bd3f671ed59e4f8
César Augusto <ces.augusto108@gmail.com>
2022-12-23 19:55:08 -0300

    fin-aid_payment_sys module created

---
+++ fin-aid_payment_sys/src/main/java/ces/augusto108/finaid_payment_sys/application/FinAidPaymentSysApplication.java
@@ -0,0 +1,11 @@
+package ces.augusto108.finaid_payment_sys.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinAidPaymentSysApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FinAidPaymentSysApplication.class, args);
+	}
+}
---
+++ fin-aid_payment_sys/src/test/java/ces/augusto108/finaid_payment_sys/FinAidPaymentSysApplicationTests.java
@@ -0,0 +1,13 @@
+package ces.augusto108.finaid_payment_sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinAidPaymentSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
