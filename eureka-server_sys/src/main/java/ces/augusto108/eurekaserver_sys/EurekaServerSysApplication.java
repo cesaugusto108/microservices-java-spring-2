22101e19282d9aefc474074b9997eeaf1d9c0bb0
César Augusto <ces.augusto108@gmail.com>
2022-12-24 19:18:53 -0300

    eureka-server_sys module created

---
+++ eureka-server_sys/src/main/java/ces/augusto108/eurekaserver_sys/EurekaServerSysApplication.java
@@ -0,0 +1,11 @@
+package ces.augusto108.eurekaserver_sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerSysApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerSysApplication.class, args);
+	}
+}
