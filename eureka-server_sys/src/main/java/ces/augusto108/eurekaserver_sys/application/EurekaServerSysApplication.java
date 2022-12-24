2406021d2dc68b450b2114c0df1b2eee1ed70bf2
César Augusto <ces.augusto108@gmail.com>
2022-12-24 19:46:30 -0300

    Eureka server module configuration / application.properties update

--- eureka-server_sys/src/main/java/ces/augusto108/eurekaserver_sys/application/EurekaServerSysApplication.java
+++ eureka-server_sys/src/main/java/ces/augusto108/eurekaserver_sys/application/EurekaServerSysApplication.java
@@ -1,11 +1,14 @@
-package ces.augusto108.eurekaserver_sys;
+package ces.augusto108.eurekaserver_sys.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerSysApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerSysApplication.class, args);
 	}
 }
+
