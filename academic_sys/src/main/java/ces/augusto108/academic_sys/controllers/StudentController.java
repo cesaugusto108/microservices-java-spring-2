487f13c1549a8f3c4efb77facac21b964dd6f10f
César Augusto <ces.augusto108@gmail.com>
2022-12-24 16:23:34 -0300

    Removed unused imports from class

--- academic_sys/src/main/java/ces/augusto108/academic_sys/controllers/StudentController.java
+++ academic_sys/src/main/java/ces/augusto108/academic_sys/controllers/StudentController.java
@@ -2,11 +2,12 @@ package ces.augusto108.academic_sys.controllers;
 
 import ces.augusto108.academic_sys.entities.Student;
 import ces.augusto108.academic_sys.services.StudentService;
-import ces.augusto108.academic_sys.services.exceptions.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
