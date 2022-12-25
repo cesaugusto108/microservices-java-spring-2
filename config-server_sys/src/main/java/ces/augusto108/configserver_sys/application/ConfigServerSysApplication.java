309d77d614cc1878948ee52a9f722cefd3ca129a
César Augusto <ces.augusto108@gmail.com>
2022-12-25 20:19:23 -0300

    config-server_sys module created

---
+++ config-server_sys/src/main/java/ces/augusto108/configserver_sys/application/ConfigServerSysApplication.java
@@ -0,0 +1,11 @@
+package ces.augusto108.configserver_sys.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerSysApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerSysApplication.class, args);
+	}
+}
---
+++ config-server_sys/src/test/java/ces/augusto108/configserver_sys/ConfigServerSysApplicationTests.java
@@ -0,0 +1,13 @@
+package ces.augusto108.configserver_sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
