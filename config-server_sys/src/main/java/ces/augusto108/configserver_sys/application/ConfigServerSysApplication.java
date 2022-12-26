1912cf049f4c2f59560cdab5161c6e1694fde5db
César Augusto <ces.augusto108@gmail.com>
2022-12-25 22:43:52 -0300

    config-server_sys configuration / fetching config from remote repo

--- config-server_sys/src/main/java/ces/augusto108/configserver_sys/application/ConfigServerSysApplication.java
+++ config-server_sys/src/main/java/ces/augusto108/configserver_sys/application/ConfigServerSysApplication.java
@@ -2,8 +2,10 @@ package ces.augusto108.configserver_sys.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerSysApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerSysApplication.class, args);
