fe20912ee02002d9b8f9ec15dd0176f31145154d
César Augusto <ces.augusto108@gmail.com>
2022-12-27 10:37:35 -0300

    administrative_sys module created

---
+++ administrative_sys/src/main/java/ces/augusto108/administrative_sys/application/AdministrativeSysApplication.java
@@ -0,0 +1,11 @@
+package ces.augusto108.administrative_sys.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministrativeSysApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AdministrativeSysApplication.class, args);
+	}
+}
