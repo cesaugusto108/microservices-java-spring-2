c0d93c68411b34744eae5088d12b8934454688f7
César Augusto <ces.augusto108@gmail.com>
2022-12-28 10:28:55 -0300

    administrative_sys module configuration

--- administrative_sys/src/main/java/ces/augusto108/administrative_sys/application/AdministrativeSysApplication.java
+++ administrative_sys/src/main/java/ces/augusto108/administrative_sys/application/AdministrativeSysApplication.java
@@ -2,8 +2,10 @@ package ces.augusto108.administrative_sys.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AdministrativeSysApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AdministrativeSysApplication.class, args);
