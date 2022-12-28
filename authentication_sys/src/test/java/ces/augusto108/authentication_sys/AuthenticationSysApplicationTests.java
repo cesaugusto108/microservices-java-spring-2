60e93e0c29522b917d9e10dd48592b6702c3edd9
César Augusto <ces.augusto108@gmail.com>
2022-12-28 20:59:37 -0300

    authentication_sys module created

---
+++ authentication_sys/src/test/java/ces/augusto108/authentication_sys/AuthenticationSysApplicationTests.java
@@ -0,0 +1,13 @@
+package ces.augusto108.authentication_sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
