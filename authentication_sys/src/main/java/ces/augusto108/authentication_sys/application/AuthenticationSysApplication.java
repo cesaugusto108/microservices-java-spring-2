bf120a41439b9b590958191e1a0c58d1e96b9e04
César Augusto <ces.augusto108@gmail.com>
2022-12-28 21:05:25 -0300

    authentication_sys module configuration as an Eureka client

--- authentication_sys/src/main/java/ces/augusto108/authentication_sys/application/AuthenticationSysApplication.java
+++ authentication_sys/src/main/java/ces/augusto108/authentication_sys/application/AuthenticationSysApplication.java
@@ -2,8 +2,10 @@ package ces.augusto108.authentication_sys.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AuthenticationSysApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticationSysApplication.class, args);
