262e94a8d5b506b06e5da5d602caebe02ce9d035
César Augusto <ces.augusto108@gmail.com>
2022-12-31 12:20:50 -0300

    Authorization to access the '/op/list/**' endpoint in authentication_sys

--- authentication_sys/src/main/java/ces/augusto108/authentication_sys/config/SecurityConfiguration.java
+++ authentication_sys/src/main/java/ces/augusto108/authentication_sys/config/SecurityConfiguration.java
@@ -3,8 +3,10 @@ package ces.augusto108.authentication_sys.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -29,5 +31,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     protected AuthenticationManager authenticationManager() throws Exception {
         return super.authenticationManager();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/op/list/**").permitAll();
     }
 }
